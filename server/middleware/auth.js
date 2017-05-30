447d0fa46b518886680ec44ff683a07b25e56bf5
Scott Moschella <scottmosch@gmail.com>
2017-05-29 22:04:22 -0700

    Add client option for heroku deploy

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient();
+const redisClient = require('redis').createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -11,6 +11,7 @@ module.exports.verify = (req, res, next) => {
 
 module.exports.session = session({
   store: new RedisStore({
+    client: redisClient,
     url: process.env.REDISCLOUD_URL
   }),
   secret: 'more laughter, more love, more life',
