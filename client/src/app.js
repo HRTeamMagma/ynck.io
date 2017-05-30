ed68cc7bd849858fdf11d032da17ba699a4e423e
anyu <ayu3500@gmail.com>
2017-05-30 14:34:25 -0700

    (feat) Add React Router, add entrypoint components

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import View from './components/View';
 
-ReactDOM.render(<h1>Hello World from React</h1>, document.getElementById('root'));
+
+ReactDOM.render(<View />, document.getElementById('root'));
