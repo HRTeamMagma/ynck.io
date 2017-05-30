24d59e4398fc61160d4466d7ff6c7c4bc06ff3f0
Helen Tang <helentang.93@gmail.com>
2017-05-30 16:18:48 -0700

    (feat) Create skeleton components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Link, BrowserRouter } from 'react-router-dom';
+
+const Footer = React.createClass ({
+  render () {
+    return (
+      <div>
+        Link to AboutUs page
+      </div>
+    );
+  }
+});
+
+export default Footer;
