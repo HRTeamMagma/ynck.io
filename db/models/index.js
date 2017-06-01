a35a557a840d7985251af111631d878220d076a9
Scott Moschella <scottmosch@gmail.com>
2017-05-31 17:00:31 -0700

    Add all the new models to bookshelf index file

--- db/models/index.js
+++ db/models/index.js
@@ -1,4 +1,6 @@
 module.exports.Auth = require('./auths');
 module.exports.Profile = require('./profiles');
 module.exports.Image = require('./images');
-module.exports.Tags = require('./tags');
+module.exports.Tag = require('./tags');
+module.exports.Shop = require('./shops');
+module.exports.Rating = require('./ratings');
