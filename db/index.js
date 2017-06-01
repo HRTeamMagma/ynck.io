6e92b0176a27ae101950a9f666d26c9741be196e
Scott Moschella <scottmosch@gmail.com>
2017-05-31 17:35:39 -0700

    Remove bookshelf-cascade-delete

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 const knex = require('knex')(require('../knexfile'));
 const bookshelf = require('bookshelf')(knex);
-var cascadeDelete = require('bookshelf-cascade-delete');
 
-bookshelf.plugin(cascadeDelete);
 bookshelf.plugin('registry');
 
 module.exports = bookshelf;
