4e32ce121d0f234057c9b2e1f59c4eba4982300f
Scott Moschella <scottmosch@gmail.com>
2017-05-31 17:00:13 -0700

    Create Image model for Bookshelf

--- db/models/images.js
+++ db/models/images.js
@@ -3,7 +3,7 @@ const bookshelf = require('../');
 const Image = bookshelf.Model.extend({
   tableName: 'images',
   profile: function() {
-    return this.belongsTo('Profile');
+    return this.belongsTo('Profile').withPivot(['image_type']);
   },
   tag: function() {
     return this.belongsToMany('Tag');
