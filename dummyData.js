cf2cf53d88783f281541f56614df44d4d37cfa67
Scott Moschella <scottmosch@gmail.com>
2017-06-01 16:46:44 -0700

    Add dummy data for the client requests

---
+++ dummyData.js
@@ -0,0 +1,89 @@
+
+module.exports.favorites = {
+  images: [
+    {
+      url: 'https://s-media-cache-ak0.pinimg.com/originals/65/df/47/65df47c1d6f5b480ff6fd573b8eb13e7.jpg',
+      tags: ['funny', 'popeye'],
+      favoriteCount: 7
+    },
+    {
+      url: 'http://img3.rnkr-static.com/user_node_img/50055/1001092218/870/peace-photo-u1.jpg',
+      tags: ['rick and morty', 'funny', 'wubalubadubdub'],
+      favoriteCount: 14
+    },
+    {
+      url: 'http://www.dubuddha.org/wp-content/uploads/2017/02/Back-Trash-Polka-Tattoo-for-Girl-by-Freezing-Tattoo-1-728x728.jpg',
+      tags: ['trash polka', 'cool', 'back tattoo'],
+      favoriteCount: 722
+    },
+    {
+      url: 'http://www.liberumstudios.com/cms/wp-content/uploads/2014/10/super-mario-tattoo-las-vegas-liberum-studios-amv.jpg',
+      tags: ['paper mario', 'nintendo', 'video game'],
+      favoriteCount: 33
+    },
+    {
+      url: 'http://tattoo-ideas.com/wp-content/uploads/2015/11/mario-8-bit-forearm-tattoo.jpg',
+      tags: ['forearm', 'mario', 'video game'],
+      favoriteCount: 4
+    }]
+};
+
+module.exports.designs =  {
+  images: [
+    {
+      url: 'https://s-media-cache-ak0.pinimg.com/736x/98/d6/83/98d68321d53d332e9d8ae0aaab2438b9.jpg',
+      tags: ['trash polka', 'awesome', 'headphones'],
+      favoriteCount: 0
+    },
+    {
+      url: 'https://s-media-cache-ak0.pinimg.com/736x/0b/b7/e8/0bb7e8705a7a4a9798938301fc992871.jpg',
+      tags: ['flower', 'abstract'],
+      favoriteCount: 4
+    },
+    {
+      url: 'http://www.freetattoodesigns.org/images/tattoo-gallery/wolf-head-design.jpg',
+      tags: ['wolf', 'flash'],
+      favoriteCount: 18
+    },
+    {
+      url: 'http://updatepedia.com/wp-content/uploads/2016/09/2096799-death-sign-isolated-on-white-for-tattoo-design.jpg',
+      tags: [],
+      favoriteCount: 112
+    },
+    {
+      url: 'https://lh5.ggpht.com/KrJXVOkgLN4A1QEnPaY-oTDp6LhtVvU8ufsXWTU-JYpBoWMc2YTqbjdRH4Xjm-sSlyd4=h900',
+      tags: ['stars', 'flash', 'basic'],
+      favoriteCount: 24
+    }
+  ]
+};
+
+module.exports.myTattoos = {
+  images: [
+    {
+      url: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-07/13/11/enhanced/webdr15/enhanced-26712-1436802498-1.jpg?downsize=715:*&output-format=auto&output-quality=auto',
+      tags: ['disney', 'up', 'pixar', 'mickey mouse'],
+      favoriteCount: 20
+    },
+    {
+      url: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-07/15/12/enhanced/webdr01/enhanced-3897-1436977511-1.jpg?downsize=715:*&output-format=auto&output-quality=auto',
+      tags: ['disney', 'lilo and stitch', 'stitch'],
+      favoriteCount: 10
+    },
+    {
+      url: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-07/15/17/enhanced/webdr10/enhanced-30058-1436997290-1.png?downsize=715:*&output-format=auto&output-quality=auto',
+      tags: ['disney', 'bow', 'minnie mouse'],
+      favoriteCount: 3
+    },
+    {
+      url: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-07/15/12/enhanced/webdr12/enhanced-12158-1436977432-1.jpg?downsize=715:*&output-format=auto&output-quality=auto',
+      tags: ['howdy howdy howdy', 'toy story', 'shark'],
+      favoriteCount: 82
+    },
+    {
+      url: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-07/15/13/enhanced/webdr15/enhanced-21279-1436979753-1.jpg?downsize=715:*&output-format=auto&output-quality=auto',
+      tags: ['mary poppins'],
+      favoriteCount: 4
+    }
+  ]
+};
