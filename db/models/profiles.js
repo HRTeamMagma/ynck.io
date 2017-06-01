a05b337bd7e16c836842e1ac8467c1868a5b73a3
Scott Moschella <scottmosch@gmail.com>
2017-05-31 17:00:54 -0700

    Update Profile model with additional attrs for bookshelf

--- db/models/profiles.js
+++ db/models/profiles.js
@@ -4,6 +4,15 @@ const Profile = bookshelf.Model.extend({
   tableName: 'profiles',
   auths: function() {
     return this.hasMany('Auth');
+  },
+  shop: function() {
+    return this.hasOne('Shop');
+  },
+  images: function() {
+    return this.hasMany('Image').withPivot(['image_type']);
+  },
+  ratings: function() {
+    return this.hasMany('Rating').withPivot(['value']);
   }
 });
 
