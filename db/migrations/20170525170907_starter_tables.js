faecac810bb0dd52f4be1a4a7595cf6edd584346
Scott Moschella <scottmosch@gmail.com>
2017-06-03 14:41:18 -0700

    Add rating and profileImage to shops

--- db/migrations/20170525170907_starter_tables.js
+++ db/migrations/20170525170907_starter_tables.js
@@ -11,6 +11,8 @@ exports.up = function(knex, Promise) {
       table.string('state', 20).nullable();
       table.string('zip', 10).nullable();
       table.string('phone', 100).nullable();
+      table.string('rating', 10).nullable();
+      table.string('profileImage', 150).nullable();
       table.timestamps(true, true);
     }),
     knex.schema.createTableIfNotExists('images', function(table) {
