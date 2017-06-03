3510b5b5ad9dd8b1e6183f365deb197f7c1da404
Devon Weldon <deveathbo@gmail.com>
2017-06-02 19:26:10 -0700

    Added map with location pin for shops page

---
+++ LatLong.js
@@ -0,0 +1,20 @@
+var NodeGeocoder = require('node-geocoder');
+
+var options = {
+  provider: 'google',
+};
+
+var ad = '1259 North La Brea Avenue Suite 200 West Hollywood, California';
+
+module.exports.latLong = (address, callback) => {
+  var geocoder = NodeGeocoder(options);
+
+  // Using callback
+  geocoder.geocode(address, function(err, res) {
+    callback(res);
+  });
+};
+
+module.exports.latLong(ad, function(res){
+  console.log(res);
+});
