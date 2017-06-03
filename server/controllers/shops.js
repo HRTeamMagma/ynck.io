3df7c120d088cc02ff2f046833e08aa5a292d0a2
Scott Moschella <scottmosch@gmail.com>
2017-06-03 15:45:41 -0700

    Add query to get shop for api calls

--- server/controllers/shops.js
+++ server/controllers/shops.js
@@ -1,6 +1,13 @@
 const models = require('../../db/models');
 const helper = require('../helpers/db_helpers');
 
-module.exports.getShopInfo = (req, res) => {
-  // models.Shop
+module.exports.getShopInfoForUser = (req, res) => {
+  models.Shop.where({id: req.query.id}).fetch().then( user => {
+    res.send(user.toJSON());
+  });
+  
+  // ({profile_id: req.query.id}).fetch({withRelated: ['shop']})
+  // .then( results => {
+  //   res.send(results.toJSON());
+  // })
 };
