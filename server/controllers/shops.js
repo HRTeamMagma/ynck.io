140c64648de7f82c0c8aa6803027d4761f48f0f7
Scott Moschella <scottmosch@gmail.com>
2017-06-03 17:20:36 -0700

    Refactor response object from shop api

--- server/controllers/shops.js
+++ server/controllers/shops.js
@@ -2,12 +2,13 @@ const models = require('../../db/models');
 const helper = require('../helpers/db_helpers');
 
 module.exports.getShopInfoForUser = (req, res) => {
-  models.Shop.where({id: req.query.id}).fetch().then( user => {
-    res.send(user.toJSON());
+  models.Profile.where({id: req.query.id}).fetch({withRelated: ['shop', 'shop.shopimages']})
+  .then ( profile => {
+    let theData = profile.related('shop').toJSON();
+    let responseObj = {};
+    responseObj.images = theData.shopimages;
+    delete theData.shopimages;
+    responseObj.shopInfo = theData;
+    res.send(responseObj);
   });
-  
-  // ({profile_id: req.query.id}).fetch({withRelated: ['shop']})
-  // .then( results => {
-  //   res.send(results.toJSON());
-  // })
 };
