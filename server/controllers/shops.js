9b635557df61ed55f31b4a4a77ea7499eb2f300b
Scott Moschella <scottmosch@gmail.com>
2017-06-03 17:31:20 -0700

    Fix shop route

--- server/controllers/shops.js
+++ server/controllers/shops.js
@@ -1,5 +1,6 @@
 const models = require('../../db/models');
 const helper = require('../helpers/db_helpers');
+const latLong = require('../../LatLong');
 
 module.exports.getShopInfoForUser = (req, res) => {
   models.Profile.where({id: req.query.id}).fetch({withRelated: ['shop', 'shop.shopimages']})
@@ -9,6 +10,20 @@ module.exports.getShopInfoForUser = (req, res) => {
     responseObj.images = theData.shopimages;
     delete theData.shopimages;
     responseObj.shopInfo = theData;
-    res.send(responseObj);
+
+    var address = responseObj.shopInfo.address1 + ' ' + responseObj.shopInfo.address2;
+    latLong.latLong(address, function(result) {
+      responseObj.lat = result[0].latitude;
+      responseObj.lon = result[0].longitude;
+      res.send(responseObj);
+    });
   });
 };
+
+
+    // var address = dummy.shop.shopInfo.address1 + ' ' + dummy.shop.shopInfo.address2;
+    // latLong.latLong(address, function(result) {
+    //   dummy.shop.lat = result[0].latitude;
+    //   dummy.shop.lon = result[0].longitude;
+    //   res.send(dummy.shop);
+    // });
