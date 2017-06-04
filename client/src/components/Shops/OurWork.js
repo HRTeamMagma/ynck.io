8d6a7408cfa64f70eb94c58b34ca72efb2a946b1
Scott Moschella <scottmosch@gmail.com>
2017-06-03 17:40:49 -0700

    Fix client code to work with new shape of response data

--- client/src/components/Shops/OurWork.js
+++ client/src/components/Shops/OurWork.js
@@ -15,7 +15,7 @@ class OurWork extends React.Component {
       <div className="shop_work">
         <h2>Our Work</h2>
         <div className="image_grid">
-        { this.props.images.map(image => <img src={image}/>) }
+        { this.props.images.map(image => <img src={image.url}/>) }
 
           
         </div>
