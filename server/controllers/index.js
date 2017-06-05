dbad62675e64101ebf52930ebfca21d01a6f55b9
Scott Moschella <scottmosch@gmail.com>
2017-06-05 15:44:21 -0700

    Add Favorites to controller export list

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,4 @@
 module.exports.Profiles = require('./profiles');
 module.exports.Images = require('./images');
 module.exports.Shops = require('./shops');
+module.exports.Favorites = require('./favorites');
