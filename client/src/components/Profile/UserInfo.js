29397cd8fb7615372337aefc5b8e1024114f9578
anyu <ayu3500@gmail.com>
2017-06-05 17:13:51 -0700

    (feat) Display edit link if logged in user views own profile

--- client/src/components/Profile/UserInfo.js
+++ client/src/components/Profile/UserInfo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const UserInfo = (props) => {
   return ( 
@@ -6,6 +8,10 @@ const UserInfo = (props) => {
         <img src={props.userInfo.profile_image} className="user_profile_image"/>
         <h2 className="user_display_name">{props.userInfo.first} {props.userInfo.last}</h2>
         <p>My tattoos are my source of strength, with which I channel sasha fierce.</p>
+        { loggedInUser.id === props.userInfo.id ?
+          <Link to={`/user/${props.userInfo.id}/edit`}>edit profile</Link>
+          : null
+        }
       </div>
   );
 };
