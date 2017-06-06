b91f4c1bc07220764226cd07362dd678db3ad9d3
Scott Moschella <scottmosch@gmail.com>
2017-06-06 11:37:39 -0700

    Add superagent configuration to webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,6 @@
 import webpack from 'webpack';
 import path from 'path';
 
-plugins.push(new webpack.DefinePlugin({ "global.GENTLY": false }));
-
 const config = {
   devtool: 'source-map',
   entry: './client/src/app',
