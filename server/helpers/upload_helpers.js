dd433e9c674c23c10af4c78acfdc41dfa13b2d03
Scott Moschella <scottmosch@gmail.com>
2017-06-06 11:42:51 -0700

    Add upload helpers to clean up api routes

---
+++ server/helpers/upload_helpers.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
+const uuidV4 = require('uuid/v4');
+
+const s3 = new AWS.S3();
+AWS.config.update(
+  {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    subregion: 'us-west-2'
+  });
+
+module.exports = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, uuidV4() + file.originalname);
+    }
+  })
+});
