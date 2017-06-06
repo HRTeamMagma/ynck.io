be750f9d754fc89e8f37522f4edbac9887f1bebc
Scott Moschella <scottmosch@gmail.com>
2017-06-06 11:39:06 -0700

    Add logic for image uploads to image controller

--- server/controllers/images.js
+++ server/controllers/images.js
@@ -2,6 +2,12 @@ const models = require('../../db/models');
 const knex = require('../../db/').knex;
 const helper = require('../helpers/db_helpers');
 
+module.exports.uploadImage = (req, res) => {
+  console.log('got an upload', req.file);
+  res.status(200).send(req.file);
+};
+
+
 module.exports.getLatestImages = (req, res) => {
   // if the user is logged in
   if (req.user) {
@@ -43,45 +49,4 @@ module.exports.getLatestImages = (req, res) => {
       res.send(500, 'Error: ' + err);
     });
   }
-  
-  // knex('images').orderBy('created_at', 'desc').where('image_type', '=', 'tattoo').limit(10)
-  // .then(result => {
-  //   res.send(result);
-  // })
-  // models.Image.query( (qb) => {
-  //   qb.where('image_type', '=', 'tattoo')
-  // })
-  // .then(results => {
-  //   console.log(results);
-  // })
-  // knex.raw(`select * from images inner join images_profiles on images_profiles.image_id = images.id and images_profiles.image_type = 'tattoo' limit 3`)
-  // .then((results) => {
-  //   console.log(results.rows);
-  //   res.send(results.rows);
-  // });
-  
-  //  inner join images_tags where images.id = images_tags.image_id inner join tags on tags.id = images_tags.tag_id;
-  // models.Image
-  // .query( (qb) => {
-  //   qb.where('image_type', '=', 'tattoo');
-  // })
-  // .orderBy('created_at')
-  // .fetchPage({
-  //   pageSize: 10,
-  //   page: 1
-  // })
-  // .then((result) => {
-  //   console.log(result);
-  // })
-
-  // .orderBy('created_at')
-  // .fetchPage({
-  //   pageSize: 10,
-  //   page: 1,
-  //   withRelated: 'tags'
-  // })
-  // .then((images) => {
-  //   console.log(images);
-  //   res.send('yo');
-  // }) 
 };
