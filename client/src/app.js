4706a6309fa1f67ffe198adfff5ce54c8efd70da
anyu <ayu3500@gmail.com>
2017-06-08 18:29:10 -0700

    (refactor) Move redux methods out of App, rename Layout to Main

--- client/src/app.js
+++ client/src/app.js
@@ -7,45 +7,19 @@ import '../../public/stylesheets/main.scss';
 
 
 import store from './store';
-import Layout from './components/Layout';
+import Main from './components/Main';
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getUserInfo = this.getUserInfo.bind(this);
-  }
 
-  componentDidMount() {
-    // this.getUserInfo();
-  }
-  getUserInfo() {
-    if (loggedInUser) {
-      this.props.fetchAllUserData('/api/profiles/user-data', loggedInUser);
-    }
-  }
   render() {
     return (
       <Provider store={store}>
-        <Layout/>
+        <Main/>
       </Provider>
-    
     );
   }
 }
 
-// const mapStateToProps = (state) => {
-//   return {
-//     userData: state.userData
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     fetchAllUserData: (url, id) => dispatch(fetchAllUserData(url, id))
-//   };
-// };
-
-// connect(mapStateToProps, mapDispatchToProps)(App);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
