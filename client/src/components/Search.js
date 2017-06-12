9550e0f22e519907d2330779324e09b7a4b2dd09
anyu <ayu3500@gmail.com>
2017-06-12 12:22:52 -0700

    (feat) Capture search input

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -4,13 +4,36 @@ import ReactDOM from 'react-dom';
 class Search extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchInput: ''
+    };
+    this.captureSearchInput = this.captureSearchInput.bind(this);
+    this.submitSearch = this.submitSearch.bind(this);
+  }
+
+  captureSearchInput(e) {
+    console.log('searchinput', this.state.searchInput);
+
+    this.setState({
+      searchInput: e.target.value
+    });
+  }
+
+  submitSearch() {
+    console.log('submit search called');
+    // GET request to db to fetch list of shops, users, tags
+    // axios.get('/search')
+    // .then((response) => {
+    //   console.log('response', response);
+    // })
   }
 
   render() {
     return (
       <div className="search" >
-        <input className="searchbox" placeholder="Search for shops, users, tags"></input>
-        <a href="#"><span className="search-icon"></span></a>
+        <input className="searchbox" placeholder="Search for shops, users, tags" onChange={(e) => this.captureSearchInput(e)}></input>
+        <span className="search-icon" onClick={ () => this.submitSearch(this.state.searchInput) }></span>
       </div>
     );
   }
