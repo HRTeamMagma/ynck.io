e9b3443041237ce7052b473855bc5291bf87fac2
Helen Tang <helentang.93@gmail.com>
2017-06-13 11:49:50 -0700

    (Feat) Clean up following reducer

--- client/reducers/reducerFollowing.js
+++ client/reducers/reducerFollowing.js
@@ -1,21 +1,4 @@
 //userFollowing
-export const getFollowingHasErrored = ( state = false, action ) => {
-  switch (action.type) {
-  case 'GET_FOLLOWING_HAS_ERRORED' :
-    return action.getFollowingHasErrored;
-  default:
-    return state;
-  }
-};
-
-export const getFollowingIsLoading = ( state = false, action ) => {
-  switch (action.type) {
-  case 'GET_FOLLOWING_IS_LOADING' :
-    return action.getFollowingIsLoading;
-  default:
-    return state;
-  }
-};
 
 export const userFollowing = ( state = [], action ) => {
   switch (action.type) {
