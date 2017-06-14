ca094b1c8e5ad495b248ffa18014066fa9e732a8
anyu <ayu3500@gmail.com>
2017-06-13 17:12:20 -0700

    (feat) Render search dropdown filter

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class Search extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchInput: ''
+      searchInput: '',
+      searchType: ''
     };
     this.captureSearchInput = this.captureSearchInput.bind(this);
   }
 
   captureSearchInput(e) {
     this.setState({
-      searchInput: e.target.value
+      [e.target.name]: e.target.value
     });
   }
 
+
   render() {
     return (
-      <div className="search" >
-        <input className="searchbox" placeholder="Search for shops, users, tags" onChange={(e) => this.captureSearchInput(e)}></input>
-        <span className="search-icon" onClick={ () => this.props.submitSearch(this.state.searchInput) }></span>
+      <div>
+      <div className="searchFilter">
+        <select className="form-control" name="searchType" onChange={(e) => this.captureSearchInput(e)} >
+          <option value="shops">Shops</option>
+          <option value="users">Users</option>
+          <option value="tags">Tags</option>
+        </select>
       </div>
+      <div className="search" >        
+        <input className="searchbox" name="searchInput" placeholder="Search for shops, users, tags" onChange={(e) => this.captureSearchInput(e)}></input>
+        <span className="search-icon" onClick={ () => this.props.submitSearch(this.state.searchInput, this.state.searchType) }></span>
+      </div>    </div>
     );
   }
 }
