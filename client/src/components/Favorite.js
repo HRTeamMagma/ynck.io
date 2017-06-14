d625e9e25d30a7f0b810656f22072b28880def8f
Helen Tang <helentang.93@gmail.com>
2017-06-13 19:32:52 -0700

    (feat) set Favorite component up to be reduxified

--- client/src/components/Favorite.js
+++ client/src/components/Favorite.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import { recentImagesFetchData, addToFavorites } from '../../actions/actionRecentImages';
-import { getUserFavorites } from '../../actions/actionFavorites';
-import { CometSpinLoader } from 'react-css-loaders';
+import { addToFavorites } from '../../actions/actionRecentImages';
+
 
 
 class Favorite extends React.Component {
@@ -13,18 +12,8 @@ class Favorite extends React.Component {
     this.addAFavorite = this.addAFavorite.bind(this);
   }
 
-  componentDidMount() {
-    this.getFavorites();
-  }
-
-  getFavorites() {
-    if (this.props.loggedInUser) {
-      this.props.getUserFavorites('/api/user/favorites', this.props.loggedInUser.id );
-    }
-  }
-
   addAFavorite(imageId, index) {
-    this.props.addToFavorites('/api/user/favorites', this.props.loggedInUser, imageId, this.props.recentImages, index);
+    
   }
 
   render() {
@@ -32,31 +21,26 @@ class Favorite extends React.Component {
       <div key={this.props.i}>
         <div className="overlay_container">
           { this.props.images.isFavorited ?
-            <img src="./../../assets/icons/favorited.png" className="heart" onClick={ () => { this.addAFavorite(images.id, i); } }/> 
-          : <img src="./../../assets/icons/heart.png" className="heart" onClick={ () => { this.addAFavorite(images.id, i); } }/> 
+            <img src="./../../assets/icons/favorited.png" className="heart" onClick={ () => { this.addAFavorite(this.props.images.id, this.props.i); } }/> 
+          : <img src="./../../assets/icons/heart.png" className="heart" onClick={ () => { this.addAFavorite(this.props.images.id, this.props.i); } }/> 
             }
         </div>
-        <img src={this.props.images.url}/>
+        <img src={this.props.images.url} />
       </div>
     ); 
   }
 }
 
-// const mapStateToProps = (state) => {
-//   return {
+const mapStateToProps = (state) => {
+  return {
+    
+  };
+};
 
-//     getFavoritesHasErrored: state.getFavoritesHasErrored,
-//     getFavoritesIsLoading: state.getFavoritesIsLoading,
-//     userFavorites: state.userFavorites
-//   };
-// };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+  };
+};
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     getUserFavorites: (url, id) => dispatch(getUserFavorites(url, id)),
-//     addToFavorites: (url, loggedInUser, imageId, imageArray, index) => dispatch(addToFavorites(url, loggedInUser, imageId, imageArray, index))
-//   };
-// };
-
-export default Favorite;
-// export default connect(mapStateToProps, mapDispatchToProps)(RecentTattoos);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
