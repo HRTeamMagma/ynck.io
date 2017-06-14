743e0dd9c6c67016d54049bc28f1f2d8d18bc98b
anyu <ayu3500@gmail.com>
2017-06-13 17:11:26 -0700

    (feat) Query appropriate DB based on search type selected

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,15 +1,34 @@
 const models = require('../../db/models');
 const helper = require('../helpers/db_helpers');
+const knex = require('../../db/').knex;
 
 
 module.exports.getSearchResults = (req, res) => {
-  models.Tag.where({name: req.query.q}).fetch({withRelated: 'image'})
-  .then(results => {
-    res.send({imageResults: results.related('image').toJSON()});
-  })
-  .catch(err => {
-    res.send(`No results were found for "${req.query.q}"`);
-  });
+  if (req.query.searchType === 'tags') {
+    models.Tag.where({name: req.query.q}).fetch({withRelated: 'image'})
+    .then(results => {
+      res.send({imageResults: results.related('image').toJSON()});
+    })
+    .catch(err => {
+      res.send(`No tattoos found for "${req.query.q}"`);
+    });
+  } else if (req.query.searchType === 'shops') {
+    knex.raw(`SELECT name, shop_image FROM shops where name like '%${req.query.q}%'`)
+      .then(results => {
+        res.send({shops: results.rows});
+      })
+      .catch(err => {
+        res.send(`No shops named for "${req.query.q}" were found`);
+      });
+  } else { // fallback to search users
+    knex.raw(`SELECT first, last, profile_image FROM profiles where first like '%${req.query.q}%' OR last like '%${req.query.q}%'`)
+      .then(results => {
+        res.send({users: results.rows});
+      })
+      .catch(err => {
+        res.send(`No users named "${req.query.q}" were found`);
+      });
+  }
 };
 
 // In raw SQL
