4f08259a9cd0d90b7cce2bea192ed7256a126043
Helen Tang <helentang.93@gmail.com>
2017-06-14 15:18:08 -0700

    (feat) Connect Feed component to Redux

--- client/src/components/Profile/Feed.js
+++ client/src/components/Profile/Feed.js
@@ -2,24 +2,29 @@ import React from 'react';
 import MyTattoos from './MyTattoos';
 import MyDesigns from './MyDesigns';
 import MyInspirations from './MyInspirations';
+import { connect } from 'react-redux';
+import { getProfileFavorites } from '../../../actions/actionProfileFavorites';
 
 class Feed extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-
-    };
+    this.addToProfileFavorites = this.addToProfileFavorites.bind(this);
   }
+
+  addToProfileFavorites(imageId, typeOfImage, i) {
+    this.props.getProfileFavorites('/api/user/favorites', loggedInUser.id, imageId, typeOfImage, i);
+  } 
+
   render() {
     return (
       <div className="user_stream">
-        { this.props.myTattoos ?
-          <MyTattoos myTattoos = {this.props.myTattoos}/>
+        { this.props.userData.tattoo ?
+          <MyTattoos myTattoos = {this.props.userData.tattoo} addToProfileFavorites={this.addToProfileFavorites}/>
           : null 
         }
-        { this.props.myDesigns ?
-          <MyDesigns myDesigns = {this.props.myDesigns}/>
+        { this.props.userData.design ?
+          <MyDesigns myDesigns = {this.props.userData.design} addToProfileFavorites={this.addToProfileFavorites}/>
           : null
         }
         { this.props.myInspirations ?
@@ -31,4 +36,18 @@ class Feed extends React.Component {
   }
 }
 
-export default Feed;
+//connect state to action
+const mapStateToProps = (state) => {
+  return {
+    userData: state.userData
+  };
+};
+
+//connects dispatch to action (fires the action)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getProfileFavorites: (url, userId, imageId, typeOfImage, i) => dispatch(getProfileFavorites(url, userId, imageId, typeOfImage, i)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Feed);
