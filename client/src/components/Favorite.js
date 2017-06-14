f94ce70760b9a479e57935b663a67307287394ce
Helen Tang <helentang.93@gmail.com>
2017-06-14 15:17:39 -0700

    (fix) Disconnect Favorite component from Redux

--- client/src/components/Favorite.js
+++ client/src/components/Favorite.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { getProfileFavorites } from '../../actions/actionProfileFavorites';
-
-
 
 class Favorite extends React.Component {
   constructor(props) {
     super(props);
-
-    this.addToProfileFavorites = this.addToProfileFavorites.bind(this);
   }
 
-  addToProfileFavorites(imageId) {
-    this.props.getProfileFavorites('/api/user/favorites', loggedInUser.id, imageId);
-  } 
 
   render() {
+    let i = this.props.i;
     return (
-      <div key={this.props.key}>
+      <div key={this.props.i}>
         <div className="overlay_container">
           { this.props.images.isFavorited ?
-            <img src="./../../assets/icons/favorited.png" className="heart" onClick={ () => { this.addToFavorites(this.props.images.id); } }/> 
+            <img src="./../../assets/icons/favorited.png" className="heart" onClick={ () => { this.props.addToProfileFavorites(this.props.images.id, this.props.typeOfImage, this.props.i); } }/> 
           : 
-            <img src="./../../assets/icons/heart.png" className="heart" onClick={ () => { this.addToProfileFavorites(this.props.images.id); } }/> 
+            <img src="./../../assets/icons/heart.png" className="heart" onClick={ () => { this.props.addToProfileFavorites(this.props.images.id, this.props.typeOfImage, this.props.i); } }/> 
             }
         </div>
         <img src={this.props.images.url} />
@@ -32,18 +23,4 @@ class Favorite extends React.Component {
   }
 }
 
-//connect state to action
-const mapStateToProps = (state) => {
-  return {
-    profileFavorites: state.profileFavorites
-  };
-};
-
-//connects dispatch to action (fires the action)
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getProfileFavorites: (url, userId, imageId) => dispatch(getProfileFavorites(url, userId, imageId)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
+export default Favorite;
