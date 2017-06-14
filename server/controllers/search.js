635c9c8d441c7bcfd44b478b722866832eab0fad
anyu <ayu3500@gmail.com>
2017-06-14 10:05:25 -0700

    (feat) Separate users search type filter from fallback case

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -11,7 +11,7 @@ module.exports.getSearchResults = (req, res) => {
       res.send({imageResults: results.related('image').toJSON()});
     })
     .catch(err => {
-      res.send({ msg:`No tattoos found for "${req.query.q}". Try, try again.`});
+      res.send({ msg: `No tattoos found for "${req.query.q}". Try, try again.`});
     });
   } else if (req.query.searchType === 'shops') {
     knex.raw(`SELECT name, shop_image FROM shops where lower(name) like '%${req.query.q}%'`)
@@ -19,20 +19,22 @@ module.exports.getSearchResults = (req, res) => {
         if (results.rows[0]) {
           res.send({shops: results.rows});
         } else {
-          res.send({ msg:`No shops named for "${req.query.q}" were found. Try, try again.`});
+          res.send({ msg: `No shops named for "${req.query.q}" were found. Try, try again.`});
         }
       });
-  } else { // fallback to search users
+  } else if (req.query.searchType === 'users') {
     knex.raw(`SELECT first, last, profile_image FROM profiles where lower(first) like '%${req.query.q}%' OR lower(last) like '%${req.query.q}%'`)
       .then(results => {
         if (results.rows[0]) {
           res.send({users: results.rows});
         } else {
-          res.send({ msg:`No users named "${req.query.q}" were found. Try, try again.`});
+          res.send({ msg: `No users named "${req.query.q}" were found. Try, try again.`});
         }
       });
+  } else {
+    res.send({ msg: `Nothing found.`});
   }
 };
 
 // In raw SQL
-// SELECT * FROM images INNER JOIN images_tags ON images.id = images_tags.image_id INNER JOIN tags ON tags.id = images_tags.tag_id WHERE tags.name = ' req.query.searchInput';
+// SELECT * FROM images INNER JOIN images_tags ON images.id = images_tags.image_id INNER JOIN tags ON tags.id = images_tags.tag_id WHERE tags.name = ' req.query.searchTerm';
