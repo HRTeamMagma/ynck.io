b32f73a46f2c60f869934785aa80b1b2bc66ddb0
Helen Tang <helentang.93@gmail.com>
2017-06-14 20:14:49 -0700

    (feat) Connect /allShop to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -60,6 +60,7 @@ class Header extends React.Component {
               <Link to="/"><h1>ynck</h1></Link>
             </div>
             <Search submitSearch={this.props.submitSearch} />
+            <a className='button' href='/allShops'>Browse</a>
             {this.props.loggedInUser ? (
               <nav>
                 <img className ='header-photo' src={this.state.userInfo.profile_image}/>
