de3e06b11f3c362258bddf94d5452f921a33cf02
anyu <ayu3500@gmail.com>
2017-06-14 23:57:29 -0700

    (feat) Render pie chart with placeholder data

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -2,12 +2,70 @@ import React from 'react';
 import ReactHighcharts from 'react-highcharts'; 
  
 const config = {
-  xAxis: {
-    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  chart: {
+    style: {
+      fontFamily: 'Arial'
+    },
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: 'pie'
+  },
+  colors: [
+    '#8f4b5a', '#4b8f5e', '#4b7c8f', '#f7a35c', '#8085e9'
+  ],
+  credits: {
+    enabled: false
+  },
+  title: {
+    text: 'Breakdown of tattoo styles'
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: false
+      },
+      showInLegend: true
+    }
   },
   series: [{
-    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+    name: 'Tattoos',
+    colorByPoint: true,
+    data: [{
+      name: 'Dotwork',
+      y: 56.33
+    }, {
+      name: 'Trash Polka',
+      y: 24.03,
+      sliced: true,
+      selected: true
+    }, {
+      name: 'Geometric',
+      y: 10.38
+    }, {
+      name: 'Watercolor',
+      y: 4.77
+    }, {
+      name: 'Neoclassical',
+      y: 0.91
+    }, {
+      name: 'Blackwork',
+      y: 0.2
+    }]
   }]
+
+  // Line graph
+  // xAxis: {
+  //   categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  // },
+  // series: [{
+  //   data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+  // }]
 };
 
 
@@ -16,12 +74,10 @@ class Stats extends React.Component {
     super(props);
   }
 
-
   render() {
     return (
       <div className="stats">
         <div className="feed_container">
-          <h4>Stats about the site</h4>
           <ReactHighcharts config = {config} />
         </div>
       </div>
