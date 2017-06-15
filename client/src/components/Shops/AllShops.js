40a0cc530e0ea8e1b696c7bd6bbfafd27c1882c4
Helen Tang <helentang.93@gmail.com>
2017-06-15 10:43:07 -0700

    (feat) create AllShop component

--- client/src/components/Shops/AllShops.js
+++ client/src/components/Shops/AllShops.js
@@ -3,12 +3,25 @@ import React from 'react';
 class AllShops extends React.Component {
   constructor (props) {
     super (props);
+  
   }
+
+  componentDidMount() {
+    this.props.fetchAllShops('/api/allShops');
+  }
+
 
   render () {
     return (
       <div> 
-      
+        <h1>HELLO</h1>
+        {this.props.allShops.data ? 
+          this.props.allShops.data.map((shop) => {
+            return <h1>shop.name</h1>;
+          })
+        :
+        null
+        }
       </div>
     );
   }
