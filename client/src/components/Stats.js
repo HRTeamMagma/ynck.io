aeb9b8be256d2d37ecb31d132c2cf48920cd2dc4
anyu <ayu3500@gmail.com>
2017-06-15 13:06:39 -0700

    (feat) Successfully retrieve img count per tag clientside

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -1,80 +1,97 @@
 import React from 'react';
 import ReactHighcharts from 'react-highcharts'; 
- 
-const config = {
-  chart: {
-    style: {
-      fontFamily: 'Arial'
-    },
-    plotBackgroundColor: null,
-    plotBorderWidth: null,
-    plotShadow: false,
-    type: 'pie'
-  },
-  colors: [
-    '#8f4b5a', '#4b8f5e', '#4b7c8f', '#f7a35c', '#8085e9'
-  ],
-  credits: {
-    enabled: false
-  },
-  title: {
-    text: 'Breakdown of tattoo styles'
-  },
-  tooltip: {
-    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  },
-  plotOptions: {
-    pie: {
-      allowPointSelect: true,
-      cursor: 'pointer',
-      dataLabels: {
-        enabled: false
-      },
-      showInLegend: true
-    }
-  },
-  series: [{
-    name: 'Tattoos',
-    colorByPoint: true,
-    data: [{
-      name: 'Dotwork',
-      y: 56.33
-    }, {
-      name: 'Trash Polka',
-      y: 24.03,
-      sliced: true,
-      selected: true
-    }, {
-      name: 'Geometric',
-      y: 10.38
-    }, {
-      name: 'Watercolor',
-      y: 4.77
-    }, {
-      name: 'Neoclassical',
-      y: 0.91
-    }, {
-      name: 'Blackwork',
-      y: 0.2
-    }]
-  }]
-
-  // Line graph
-  // xAxis: {
-  //   categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  // },
-  // series: [{
-  //   data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
-  // }]
-};
+import axios from 'axios';
 
 
 class Stats extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      totalPicsPerTag: {}
+    };
+    this.getStats = this.getStats.bind(this);
+  }
+
+  componentDidMount() {
+    this.getStats();
+  }
+
+  getStats() {
+    axios.get('/api/stats')
+      .then(response => {
+        var totalPicsPerTag = {};
+        response.data.map(tag => {
+          totalPicsPerTag[tag.tag_id] = tag.count;
+        });
+        this.setState({
+          totalPicsPerTag
+        });
+      });
   }
 
   render() {
+    this.state.totalPicsPerTag ? 
+      console.log('in render totalPicsPerTag', this.state.totalPicsPerTag)
+      : null;
+    const config = {
+      chart: {
+        style: {
+          fontFamily: 'Arial'
+        },
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      colors: [
+        '#8f4b5a', '#4b8f5e', '#4b7c8f', '#f7a35c', '#8085e9',
+        '#2a2a2a', '#26f683'
+      ],
+      credits: {
+        enabled: false
+      },
+      title: {
+        text: 'Breakdown of tattoo styles'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false
+          },
+          showInLegend: true
+        }
+      },
+      series: [{
+        name: 'Tattoos',
+        colorByPoint: true,
+        data: [{
+          name: 'Dotwork',
+          y: 56.33
+        }, {
+          name: 'Trash Polka',
+          y: 24.03,
+          sliced: true,
+          selected: true
+        }, {
+          name: 'Geometric',
+          y: 10.38
+        }, {
+          name: 'Watercolor',
+          y: 4.77
+        }, {
+          name: 'Neoclassical',
+          y: 0.91
+        }, {
+          name: 'Blackwork',
+          y: 0.2
+        }]
+      }]
+    };
     return (
       <div className="stats">
         <div className="feed_container">
@@ -84,5 +101,6 @@ class Stats extends React.Component {
     );
   }
 }
+
 
 export default Stats;
