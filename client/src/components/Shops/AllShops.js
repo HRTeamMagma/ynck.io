10bb26349f16dcbe7f53a74bc15b8f0215ae82cf
Helen Tang <helentang.93@gmail.com>
2017-06-14 20:17:08 -0700

    (feat) Create base shop dump component

---
+++ client/src/components/Shops/AllShops.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class AllShops extends React.Component {
+  constructor (props) {
+    super (props);
+  }
+
+  render () {
+    return (
+      <div> 
+      
+      </div>
+    );
+  }
+
+}
+
+export default AllShops;
