e58811e74f4cf56b216ec78b43863004f7dacfcf
Helen Tang <helentang.93@gmail.com>
2017-06-15 14:27:23 -0700

    (feat) Only render following component if user is following people

--- client/src/components/Profile/Following.js
+++ client/src/components/Profile/Following.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 const Following = (props) => {
   return (
-    <div className="following">
-      <div>
-        Following
+    props.usersFollowing.length > 0 ? (
+      <div className="following">
+        <div>
+          Following
+        </div>
+        {props.usersFollowing.map((users, i) => {
+          return (
+            <span key={i}>
+              <a href={`/user/${users.id}`} ><img src={users.profile_image} /></a>
+            </span>
+          );
+        })}
       </div>
-      {props.usersFollowing.map((users, i) => {
-        return (
-          <span key={i}>
-            <a href={`/user/${users.id}`} ><img src={users.profile_image} /></a>
-          </span>
-        );
-      })}
-    </div>
+    )
+    :
+    null
   );
 };
 
