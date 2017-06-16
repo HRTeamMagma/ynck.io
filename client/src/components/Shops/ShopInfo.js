3ecc335b7d1c1adc2dc51c2e2499aa6f6d2be66c
Scott Moschella <scottmosch@gmail.com>
2017-06-15 20:01:10 -0700

    Style rating widget

--- client/src/components/Shops/ShopInfo.js
+++ client/src/components/Shops/ShopInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Rating from 'react-rating';
 
 class ShopInfo extends React.Component {
   constructor(props) {
@@ -12,6 +12,15 @@ class ShopInfo extends React.Component {
   render () {
     return (
       <div className="shop_info">
+        <Rating 
+        start={0} 
+        stop={5} 
+        initialRate={parseFloat(this.props.rating)} 
+        empty={<img src="https://s3-us-west-1.amazonaws.com/media.ynck.com/empty_star.png" className="icon" />}
+        full={<img src="https://s3-us-west-1.amazonaws.com/media.ynck.com/filled_star.png" className="icon" />}
+        fractions={2}/>
+        
+        
         <p className="info_element"><strong>Rating</strong>: {this.props.rating}</p>    
         <p className="info_element">{this.props.address1}</p>    
         <p className="info_element">{this.props.city}, {this.props.state}</p> 
